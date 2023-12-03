d8e9c7783159aa0d40e0533cd2a0ae7377616a47
ananyaduvvuri <ananyaduvvuri@gmail.com>
2023-12-04 01:06:45 +0530

    ll

--- controllers/login.js
+++ controllers/login.js
@@ -45,53 +45,6 @@ router.use(bodyParser.urlencoded({extended : true}));
 router.use(bodyParser.json());
 
 
-var express = require ('express');
-var home = require('./home');
-var mysql =require('mysql');
-var session = require ('express-session');
-var router = express.Router();
-var bodyParser = require('body-parser');
-var db = require.main.require ('./models/db_controller');
-var  sweetalert = require('sweetalert2');
-const { check, validationResult } = require('express-validator');
-
-
-
-
-router.get('/', function(req ,res){
-
-    res.render('login.ejs');
-});
-
-var con = mysql.createConnection({
-
-    host: 'mysqlnode.mysql.database.azure.com',
-    user: 'adminlogin@mysqlnode',
-    password: 'Ann280902',
-    database: ' ',
-    port: 3306, // Default MySQL port
-    ssl: {
-      rejectUnauthorized: true
-    }
-});
-
-
-
-
-
-router.use(session({
-
-    secret: 'secret',
-    resave : true ,
-    saveUninitialized : true 
-
-}));
-
-
-router.use(bodyParser.urlencoded({extended : true}));
-router.use(bodyParser.json());
-
-
 router.post('/',[
     check('username').notEmpty().withMessage("Username is reequired"),
     check('password').notEmpty().withMessage("Password is reequired")
@@ -107,7 +60,7 @@ router.post('/',[
 
     if (username && password){
         con.query('select * from users where username = ? and password = ?' , [username, password], function(error , results , fields){
-            if (results.length > 0){
+            if (Array.isArray(results) && results.length > 0) {
                 
                 request.session.loggedin = true ; 
                 request.session.username = username;
@@ -131,5 +84,4 @@ router.post('/',[
 
 });
 
-module.exports = router;
 module.exports = router;
